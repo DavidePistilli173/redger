a26e03c65afbfcbd53b8526882fc79676839fcfb
DavidePistilli173 <davidepistilli173@gmail.com>
2023-08-29 20:22:20 +0200

    Test new logging features.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rwlog::{Level, Logger, Target};
+
 fn main() {
-    println!("Hello, world!");
+    let logger = Logger::new(Level::Trace, Target::Console);
+    rwlog::trace!(&logger, "Hello Debug Trace!");
+    rwlog::info!(&logger, "Hello Debug Info!");
+    rwlog::warn!(&logger, "Hello Debug Warn!");
+    rwlog::err!(&logger, "Hello Debug Error!");
+    rwlog::fatal!(&logger, "Hello Debug Fatal!");
+
+    rwlog::rel_trace!(&logger, "Hello Trace!");
+    rwlog::rel_info!(&logger, "Hello Info!");
+    rwlog::rel_warn!(&logger, "Hello Warn!");
+    rwlog::rel_err!(&logger, "Hello Error!");
+    rwlog::rel_fatal!(&logger, "Hello Fatal!");
 }
