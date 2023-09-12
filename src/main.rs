12952965f6975d6a07ff7a48aab36b8324c1e991
DavidePistilli173 <davidepistilli173@gmail.com>
2023-09-12 20:04:47 +0200

    Send network log messages.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use rwlog::{Level, Logger, Target};
+use rwlog::{Level, Logger, NetworkSettings};
 
 fn main() {
-    let logger = Logger::new(Level::Trace, Target::Console);
+    let logger = Logger::to_network(
+        Level::Trace,
+        &NetworkSettings {
+            local_socket: "127.0.0.1:1234".to_string(),
+            destination_socket: "127.0.0.1:2345".to_string(),
+        },
+    );
     rwlog::trace!(&logger, "Hello Debug Trace!");
     rwlog::info!(&logger, "Hello Debug Info!");
     rwlog::warn!(&logger, "Hello Debug Warn!");
     rwlog::err!(&logger, "Hello Debug Error!");
-    rwlog::fatal!(&logger, "Hello Debug Fatal!");
 
     rwlog::rel_trace!(&logger, "Hello Trace!");
     rwlog::rel_info!(&logger, "Hello Info!");
     rwlog::rel_warn!(&logger, "Hello Warn!");
     rwlog::rel_err!(&logger, "Hello Error!");
-    rwlog::rel_fatal!(&logger, "Hello Fatal!");
 }
