780cc3b0a82a8e592ec2115e0d4437d4bb8ab990
DavidePistilli173 <davidepistilli173@gmail.com>
2023-09-16 17:06:17 +0200

    Add network logging test.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-use rwlog::{Level, Logger, NetworkSettings};
+use crossbeam::channel::RecvTimeoutError;
+use rwlog::{receiver, sender, Level};
+use std::time::Duration;
 
 fn main() {
-    let logger = Logger::to_network(
+    let logger_network_sender = sender::Logger::to_network(
         Level::Trace,
-        &NetworkSettings {
+        &sender::NetworkSettings {
             local_socket: "127.0.0.1:1234".to_string(),
             destination_socket: "127.0.0.1:2345".to_string(),
         },
     );
-    rwlog::trace!(&logger, "Hello Debug Trace!");
-    rwlog::info!(&logger, "Hello Debug Info!");
-    rwlog::warn!(&logger, "Hello Debug Warn!");
-    rwlog::err!(&logger, "Hello Debug Error!");
 
-    rwlog::rel_trace!(&logger, "Hello Trace!");
-    rwlog::rel_info!(&logger, "Hello Info!");
-    rwlog::rel_warn!(&logger, "Hello Warn!");
-    rwlog::rel_err!(&logger, "Hello Error!");
+    let logger_console = sender::Logger::to_console(Level::Trace);
+
+    let logger_receiver = receiver::Logger::new(Level::Trace, "127.0.0.1:2345");
+
+    rwlog::trace!(&logger_network_sender, "Hello Debug Trace!");
+    rwlog::info!(&logger_network_sender, "Hello Debug Info!");
+    rwlog::warn!(&logger_network_sender, "Hello Debug Warn!");
+    rwlog::err!(&logger_network_sender, "Hello Debug Error!");
+
+    rwlog::rel_trace!(&logger_network_sender, "Hello Trace!");
+    rwlog::rel_info!(&logger_network_sender, "Hello Info!");
+    rwlog::rel_warn!(&logger_network_sender, "Hello Warn!");
+    rwlog::rel_err!(&logger_network_sender, "Hello Error!");
+
+    let mut done = false;
+    while !done {
+        match logger_receiver.next_message(Some(Duration::from_millis(1000))) {
+            Ok(x) => {
+                rwlog::rel_info!(&logger_console, "{:?}", x);
+            }
+            Err(RecvTimeoutError::Disconnected) => {
+                done = true;
+            }
+            Err(RecvTimeoutError::Timeout) => {
+                rwlog::rel_info!(&logger_console, "Timeout reached.");
+            }
+        }
+    }
 }
