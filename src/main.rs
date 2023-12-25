b6055669a31c54ebfe09e971d8b1283c14bea562
DavidePistilli173 <davidepistilli173@gmail.com>
2023-12-25 09:53:20 +0100

    Complete extraction of UI logic from rwgfx.

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,7 @@ fn run(
                 }
             }
             Event::RedrawRequested(window_id) if window_id == window.id() => {
-                match context.render(|_id, frame_context| button.draw(frame_context)) {
+                match context.render(|queue, render_pass, _| button.draw(queue, render_pass)) {
                     Ok(_) => (),
                     Err(RenderError::SurfaceInvalid) => {
                         context.resize(window.inner_size().width, window.inner_size().height)
